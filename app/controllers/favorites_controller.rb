fca9c352ae6e55f059c58b508976c6822b54e4b9
EC2 Default User <ec2-user@ip-172-31-21-177.ec2.internal>
2021-08-03 08:33:30 +0000

    [Update]ouyou-6

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,17 +1,28 @@
 class FavoritesController < ApplicationController
   before_action :authenticate_user!
+  before_action :book_params
+  # 投稿のidを取得
 
   def create
-    @book = Book.find(params[:book_id])
-    favorite = @book.favorites.new(user_id: current_user.id)
-    favorite.save
-    redirect_to request.referer
+    @favorite = Favorite.create(user_id: current_user.id, book_id: @book.id)
+    
+    # favorite = @book.favorites.new(user_id: current_user.id)
+    # favorite.save
+    # redirect_to request.referer
   end
 
   def destroy
-    @book = Book.find(params[:book_id])
-    favorite = @book.favorites.find_by(user_id: current_user.id)
-    favorite.destroy
-    redirect_to request.referer
+    @favorite = Favorite.find_by(user_id: current_user.id, book_id: @book.id).destroy
+
+    # favorite = @book.favorites.find_by(user_id: current_user.id)
+    # favorite.destroy
+    # redirect_to request.referer
   end
+  
+  private
+  
+  def book_params
+    @book = Book.find(params[:book_id])
+  end
+  
 end
