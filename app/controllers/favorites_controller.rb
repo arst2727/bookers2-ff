635ce7dc485b5b533ba87a292406189ae973d3cd
arst2727 <yanagit4729@gmail.com>
2021-08-03 08:44:38 +0000

    [Update]ouyou-6

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -5,18 +5,10 @@ class FavoritesController < ApplicationController
 
   def create
     @favorite = Favorite.create(user_id: current_user.id, book_id: @book.id)
-    
-    # favorite = @book.favorites.new(user_id: current_user.id)
-    # favorite.save
-    # redirect_to request.referer
   end
 
   def destroy
     @favorite = Favorite.find_by(user_id: current_user.id, book_id: @book.id).destroy
-
-    # favorite = @book.favorites.find_by(user_id: current_user.id)
-    # favorite.destroy
-    # redirect_to request.referer
   end
   
   private
