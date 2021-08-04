340f05171f9467c3f9403239aa08b93ea7381f9c
arst2727 <yanagit4729@gmail.com>
2021-08-04 06:01:23 +0000

    [Fix]ouyou-6

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -3,21 +3,18 @@ class BookCommentsController < ApplicationController
 
 	def create
 		@book = Book.find(params[:book_id])
-		@book_comment = BookComment.new(book_comment_params)
-		@book_comment.book_id = @book.id
-		@book_comment.user_id = current_user.id
-		if @book_comment.save
-			render 'books/comments/create'
-		else
-			render 'books/show'
-		end
+    	@book_comment = BookComment.new(book_comment_params)
+    	@book_comment.book_id = @book.id
+    	@book_comment.user_id = current_user.id
+	    unless @book_comment.save
+	      render 'error'
+	    end
 	end
 
 	def destroy
 		@book = Book.find(params[:book_id])
-  		@book_comment = @book.book_comments.find(params[:id])
-		@book_comment.destroy
-		render 'books/comments/destroy'
+  		book_comment = @book.book_comments.find(params[:id])
+		book_comment.destroy
 	end
 
 	private
